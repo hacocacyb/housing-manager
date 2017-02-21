d29f75b5e204198092ca99fdbbec4017c8262e4a
Eric Weiler <ejweiler@gmail.com>
2017-02-21 16:12:24 -0600

    move database to server project

--- database/sql/getBuildings.sql
+++
@@ -1,9 +0,0 @@
-select *,
-(Select count(*) from "Bed" where "Bed"."BuildingId" = b."Id") as "BedCount",
-(select count(*) from "Visit" v
-  where v."BuildingId" = b."Id"
-  and v."Intake" < now()
-  and (v."Outtake" is null or v."Outtake" > now())
-) as "Occupied"
-
-from "Building" b
